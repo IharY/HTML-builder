28a6718e15da77c49b50babb62be9addbbe0629b
Igor <igor@gmail.com>
2022-11-08 02:22:32 +0300

    feat: add 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+let folderPath = path.join(__dirname, "secret-folder");
+
+fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+  files.forEach((file) => {
+    if (file.isFile()) {
+      fs.stat(path.join(folderPath, file.name), (err, fileStat) => {
+        if (err) throw err;  
+        console.log(path.basename(file.name))
+        console.log(path.extname(file.name))
+        console.log(path.basename(file.name, path.extname(file.name)) + " - " + path.extname(file.name).slice(1) + " - " + (fileStat.size / 1024).toFixed(3) + "kb");
+      });
+    }
+  });
+});
